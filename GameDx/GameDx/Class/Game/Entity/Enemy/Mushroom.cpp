18b68f00dc2054dd02e7b7545fe7d397a6d1dcb8
Trung Lee <trunglm13@gmail.com>
2016-07-31 12:52:43 +0700

    Add filter Enemies. Add Mushroom. Add Reource Image. Delete some image which isnt unnecessary.

---
+++ GameDx/GameDx/Class/Game/Entity/Enemy/Mushroom.cpp
@@ -0,0 +1,15 @@
+//#include "Mushroom.h"
+//
+//
+//CMushroom::CMushroom()
+//{
+//}
+//
+//
+//CMushroom::~CMushroom()
+//{
+//}
+//
+//bool	CMushroom::loadSprite(){
+//	this->m_listSprite.push_back(new CSprite(CInfo))
+//}
